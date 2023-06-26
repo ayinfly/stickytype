d6cb10d4c26050a336df5b48646509fea6b8e6d2
ayinfly <ayinflyschool@gmail.com>
2023-06-26 14:05:39 -0700

    create templates for home and about pages

--- django_project/type/views.py
+++ django_project/type/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts = [
+    {
+        'author': 'Help',
+        'title': 'Help title',
+        'content': 'whooh',
+        'date_posted': 'idk'
+    },
+    {
+        'author': 'Help 2',
+        'title': 'Help title',
+        'content': 'whooh',
+        'date_posted': 'idk'
+    },
+    {
+        'author': 'Help 3',
+        'title': 'Help title',
+        'content': 'whooh',
+        'date_posted': 'idk'
+    }
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'type/home.html', context)
+
+def about(request):
+    return render(request, 'type/about.html')
