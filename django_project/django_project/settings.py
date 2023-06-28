968881eeb95a9d18ee0563ceb4508090da6ca168
ayinfly <ayinflyschool@gmail.com>
2023-06-27 21:31:05 -0700

    add register page

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'type.apps.TypeConfig',
     "django.contrib.admin",
     "django.contrib.auth",
