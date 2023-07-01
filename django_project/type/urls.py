1e122761f2c9e6f61a16d4471bcc46205714875c
ayinfly <ayinflyschool@gmail.com>
2023-07-01 12:25:49 -0700

    add create views

--- django_project/type/urls.py
+++ django_project/type/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import StatListView, StatDetailView, StatCreateView
+from .views import StatListView, StatDetailView, StatCreateView, StatUpdateView, StatDeleteView
 from . import views
 
 urlpatterns = [
     path("", views.home, name='type-home'),
     path('stats/<int:pk>/', StatDetailView.as_view(), name='stat-detail'),
     path('stats/new/', StatCreateView.as_view(), name='stat-create'),
+    path('stats/<int:pk>/update/', StatUpdateView.as_view(), name='stat-update'),
+    path('stats/<int:pk>/delete/', StatDeleteView.as_view(), name='stat-delete'),
     path("about/", views.about, name='type-about'),
     path("stats/", StatListView.as_view(), name='type-stats'),
 ]
--- django_project/type/views.py
+++ django_project/type/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Stat
 
 def home(request):
@@ -20,7 +21,7 @@ class StatListView(ListView):
 class StatDetailView(DetailView):
     model = Stat
 
-class StatCreateView(CreateView):
+class StatCreateView(LoginRequiredMixin, CreateView):
     model = Stat
     fields = ['wpm_total', 'wpm_raw', 'accuracy', 'mode']
     for ch in 'qwertyuiopasdfghjklzxcvbnm':
@@ -29,6 +30,32 @@ class StatCreateView(CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+class StatUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Stat
+    fields = ['wpm_total', 'wpm_raw', 'accuracy', 'mode']
+    for ch in 'qwertyuiopasdfghjklzxcvbnm':
+        fields.append('wpm_' + ch)
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class StatDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Stat
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def stats(request):
     context = {
