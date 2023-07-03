2c34b47364a878b9e5d557902fa5128f4d1b8939
ayinfly <ayinflyschool@gmail.com>
2023-07-02 17:58:01 -0700

    added ajax post functionality

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -8,7 +8,6 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
             messages.success(request, f'your account has been created! you are now able to login')
             return redirect('login')
     else:
