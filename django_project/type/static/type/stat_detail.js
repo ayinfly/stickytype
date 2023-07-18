f1bc64b582afe7cdabc35844466f068eaecc32b0
ayinfly <ayinflyschool@gmail.com>
2023-07-18 11:16:57 -0700

    add all three lines on graph

--- django_project/type/static/type/stat_detail.js
+++ django_project/type/static/type/stat_detail.js
@@ -1,44 +1,67 @@
 // Combine arrays into an array of dictionaries
-const combinedArray = graph_date.map((x, index) => {
-  return { x: x, y: graph_wpm[index] };
-});
+function dataMaker(dataPoints) {
+    const scatterData = dataPoints.reverse().map((value, index) => {
+        return { x: index, y: value };
+    });
+    return scatterData
+}
 
-const scatterData = graph_wpm.reverse().map((value, index) => {
-    return { x: index, y: value };
-});
-
-const sumX = scatterData.reduce((sum, point) => sum + Number(point.x), 0);
-const sumY = scatterData.reduce((sum, point) => sum + Number(point.y), 0);
-const meanX = sumX / scatterData.length;
-const meanY = sumY / scatterData.length;
-
-const numerator = scatterData.reduce((sum, point) => sum + (point.x - meanX) * (point.y - meanY), 0);
-const denominator = scatterData.reduce((sum, point) => sum + (point.x - meanX) ** 2, 0);
-
-const slope = numerator / denominator;
-const intercept = meanY - slope * meanX;
-
-const lineData = scatterData.map(point => {
-    return {
-        x: point.x,
-        y: slope * point.x + intercept
-    };
-});
-
-const wpmData = scatterData;
-const wpmLineData = lineData;
+function lineMaker(dataPoints) {
+    const scatterData = dataMaker(dataPoints.reverse());
+    const sumX = scatterData.reduce((sum, point) => sum + Number(point.x), 0);
+    const sumY = scatterData.reduce((sum, point) => sum + Number(point.y), 0);
+    const meanX = sumX / scatterData.length;
+    const meanY = sumY / scatterData.length;
+    const numerator = scatterData.reduce((sum, point) => sum + (point.x - meanX) * (point.y - meanY), 0);
+    const denominator = scatterData.reduce((sum, point) => sum + (point.x - meanX) ** 2, 0);
+    const slope = numerator / denominator;
+    const intercept = meanY - slope * meanX;
+    const lineData = scatterData.map(point => {
+        return {
+            x: point.x,
+            y: slope * point.x + intercept
+        };
+    });
+    return lineData;
+}
 
 const data = {
     datasets: [{
         label: 'wpm_total',
-        data: wpmData,
+        data: dataMaker(graph_wpm),
         backgroundColor: 'rgb(13,110,253)'
     },
     {
-      label: 'lsrl',
-      data: wpmLineData,
+      label: 'lsrl_wpm',
+      data: lineMaker(graph_wpm),
       fill: false,
       borderColor: 'rgb(13,110,253,1)',
+      borderWidth: 1,
+      showLine: true
+    },
+    {
+        label: 'wpm_raw',
+        data: dataMaker(graph_raw),
+        backgroundColor: 'rgb(220,53,69)'
+    },
+    {
+      label: 'lsrl_raw',
+      data: lineMaker(graph_raw),
+      fill: false,
+      borderColor: 'rgb(220,53,69,1)',
+      borderWidth: 1,
+      showLine: true
+    },
+    {
+        label: 'accuracy',
+        data: dataMaker(accuracy),
+        backgroundColor: 'rgb(25,135,84)'
+    },
+    {
+      label: 'lsrl_acc',
+      data: lineMaker(accuracy),
+      fill: false,
+      borderColor: 'rgb(25,135,84,1)',
       borderWidth: 1,
       showLine: true
     }],
@@ -58,7 +81,9 @@ const config = {
         legend: {
             labels: {
                 filter: function(label) {
-                    if (label.text === 'wpm_total') return true;
+                    if (label.text === 'wpm_tortal') return true;
+                    if (label.text === 'wpm_raw') return true;
+                    if (label.text === 'accuracy') return true;
                 }
             }
         }
